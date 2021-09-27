5435d770900f020f3b48fa68632fc12e083f69b6
menal-shams <mshams1@umd.edu>
2021-09-26 23:25:25 -0400

    lab 4

---
+++ client/lab_4/script.js
@@ -0,0 +1,40 @@
+let slidePosition = 0;
+let slides = document.querySelectorAll(".carousel_item");
+let totalSlides = slides.length;
+
+document.querySelector("#next-button").addEventListener("click", function() {
+    moveToNextSlide();
+});
+
+document.querySelector("#prev-button").addEventListener("click", function() {
+    moveToPrevSlide();
+});
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove("visible");
+    }
+
+    slides[slidePosition].classList.add("visible");
+}
+
+function moveToNextSlide() {
+    if (slidePosition == totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    if (slidePosition == 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePostion--;
+    }
+
+    updateSlidePosition();
+}
+
